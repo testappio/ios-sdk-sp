351d0d43ca3225fa7d0411d9e2b461c797cc6bd4
Lisnic Victor <gearasim.kisslin@gmail.com>
2022-07-05 17:32:17 +0300

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -16,6 +16,14 @@ let package = Package(
         .package(url: "https://github.com/tus/TUSKit.git", from: "3.0.0"),
     ],
     targets: [
+        .target(name: "TestAppIOSDKWrapper",
+                dependencies: [
+                    .target(name: "TestAppIOSDK"),
+                    .product(name: "TinyConstraints"),
+                    .product(name: "KeychainSwift"),
+                    .product(name: "TUSKit")
+                ]
+        ),
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "TestAppIOSDK", path: "TestAppIOSDK.xcframework"),
