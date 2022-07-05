92ccf191cfe07228fac445019b00fe4882489a8b
Lisnic Victor <gearasim.kisslin@gmail.com>
2022-07-05 05:15:11 +0300

    Add dependencies to package description

--- Package.swift
+++ Package.swift
@@ -10,6 +10,11 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "TestAppIOSDK", targets: ["TestAppIOSDK"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/roberthein/TinyConstraints.git", from: "4.0.0"),
+        .package(url: "https://github.com/evgenyneu/keychain-swift.git", from: "20.0.0"),
+        .package(url: "https://github.com/tus/TUSKit.git", from: "3.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
